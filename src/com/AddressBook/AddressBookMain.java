a6fcd347ffa805841bbf479ec543102dc993a162
Vishruti Patil <vishruti2908@gmail.com>
2023-04-10 17:11:29 +0530

    Program modified to sort contacts Alphabetically by first name

--- src/com/AddressBook/AddressBookMain.java
+++ src/com/AddressBook/AddressBookMain.java
@@ -8,52 +8,24 @@ class AddressBook {
     public AddressBook() {
         this.people = new ArrayList<Contact>();
     }
-
     public void addContact(Contact contact) {
         this.people.add(contact);
     }
-
-    public List<Contact> search(String city, String state) {
-        return people.stream().filter(person -> person.getCity().equals(city) && person.getState().equals(state)).collect(Collectors.toList());
+    public List<Contact> getContact() {
+        return people;
     }
 
-    public Map<String, Long> countByCity() {
-        return people.stream().collect(Collectors.groupingBy(Contact::getCity, Collectors.counting()));
-    }
-
-    public Map<String, Long> countByState() {
-        return people.stream().collect(Collectors.groupingBy(Contact::getState, Collectors.counting()));
-    }
 }
 public class AddressBookMain {
     public static void main(String[] args) {
-        AddressBook addressBook1 = new AddressBook();
-        addressBook1.addContact(new Contact("Vishruti","Patil","D207","Indore","MP","452016","000","abc"));
-        addressBook1.addContact(new Contact("A","B","C","D","E","F","G","H"));
-        addressBook1.addContact(new Contact("Q","W","E","R","T","Y","U","I"));
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact(new Contact("Vishruti","Patil","D207","Indore","MP","452016","000","abc"));
+        addressBook.addContact(new Contact("A","B","C","D","E","F","G","H"));
+        addressBook.addContact(new Contact("Q","W","E","R","T","Y","U","I"));
 
-        AddressBook addressBook2 = new AddressBook();
-        addressBook2.addContact(new Contact("Ashu","Chouhan","123","XYZ","UP","1324","12345","fm"));
-        addressBook2.addContact(new Contact("a","b","c","d","e","f","g","h"));
-        addressBook2.addContact(new Contact("q","w","e","r","t","y","u","i"));
-
-        List<AddressBook> addressBooks = Arrays.asList(addressBook1, addressBook2);
-
-        // count by city
-        Map<String, Long> countByCity = addressBooks.stream()
-                .flatMap(addressBook -> addressBook.countByCity().entrySet().stream())
-                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingLong(Map.Entry::getValue)));
-
-        System.out.println("Count by City:");
-        countByCity.forEach((city, count) -> System.out.println(city + ": " + count));
-
-        // count by state
-        Map<String, Long> countByState = addressBooks.stream()
-                .flatMap(addressBook -> addressBook.countByState().entrySet().stream())
-                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingLong(Map.Entry::getValue)));
-
-        System.out.println("Count by State:");
-        countByState.forEach((state, count) -> System.out.println(state + ": " + count));
+        List<Contact> sortedPeople = addressBook.getContact().stream().sorted(Comparator.comparing(Contact::getFirstName)).collect(Collectors.toList());
+        System.out.println("Sorted Address Book by Name:");
+        sortedPeople.forEach(System.out::println);
     }
 }
     class Contact {
